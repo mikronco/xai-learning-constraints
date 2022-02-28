53af5b99640390704ce46f651b5baabfb1866b77
mikronco <ronco.michele@yahoo.it>
2022-02-28 14:40:22 +0100

    added MoRF metric

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -5,8 +5,10 @@ Created on Sun Feb 27 17:41:11 2022
 @author: unknown
 """
 
+import numpy as np
 import torch
-
+from captum.attr import Saliency
+from torch.nn import Softmax 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -24,3 +26,23 @@ def accuracy(model, loaders):
     acc = correct/len(loaders["test"])
     return acc
 
+
+
+def MoRF(model, loaders, perc):
+    softm = Softmax()
+    n = 0
+    drops = np.zeros(len(perc))
+    grad = Saliency(model)
+    for images, labels in loaders['test']:
+        n+=1
+        images, labels = images.to(device), labels.to(device)
+        attr = grad.attribute(images, target=labels.item())
+        attr = attr.squeeze()
+        for j in range(len(perc)):
+            q3, q1 = np.percentile(attr[attr!=0].flatten().cpu().detach().numpy(), [perc[j], 100-perc[j]])
+            maskpos = torch.ones((images.shape[-1], images.shape[-1]))
+            maskpos[attr>q3]=0
+            d = softm(model(images.cuda()*maskpos.cuda())).cpu().detach().numpy()[0,torch.argmax(softm(model(images.cuda())))]/torch.max(softm(model(images.cuda()))).cpu().detach().numpy()
+            drops[j] += d
+    
+    return drops/n
